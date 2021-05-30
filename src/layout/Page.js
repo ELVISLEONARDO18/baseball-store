a81ee838de4a59d901f749904157207b08af2292
Elvis <elvisleonardo18@gmail.com>
2021-05-30 16:18:38 -0400

    navegamos con HashRouter

--- src/layout/Page.js
+++ src/layout/Page.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Layout } from 'antd';
-import { HeaderContainer } from '../containers/HeaderContainer';
-import { Navigator } from '../navigator/Navigator';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { FooterContainer } from '../containers/FooterContainer';
+import React from "react";
+import { Layout } from "antd";
+import { HeaderContainer } from "../containers/HeaderContainer";
+import { Navigator } from "../navigator/Navigator";
+import { HashRouter as Router } from "react-router-dom";
+import { FooterContainer } from "../containers/FooterContainer";
 
 export const Page = () => {
-    return (
-        <Layout className="layout">
-            <Router>
-                <HeaderContainer/>
-                <Navigator/>
-            </Router>
-            <FooterContainer/>
-        </Layout>
-    )
-}
+  return (
+    <Layout className="layout">
+      <Router>
+        <HeaderContainer />
+        <Navigator />
+      </Router>
+      <FooterContainer />
+    </Layout>
+  );
+};
