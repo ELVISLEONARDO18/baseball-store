9a0abdcf96e7690bec33bc970d99ac05a879c010
Elvis <elvisleonardo18@gmail.com>
2021-05-30 16:24:29 -0400

    cambiamos navegacion a BrowserRouter

--- src/layout/Page.js
+++ src/layout/Page.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Layout } from "antd";
 import { HeaderContainer } from "../containers/HeaderContainer";
 import { Navigator } from "../navigator/Navigator";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { FooterContainer } from "../containers/FooterContainer";
 
 export const Page = () => {
